5b950b80c02b5a14bdd9fb11871c16d958154921
NilsonDeon <nilsondeon01@gmail.com>
2024-08-16 15:48:13 -0300

    implementação 2

---
+++ implementacao-II-PAA/metodo-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "randomVector.h"
+
+using namespace std;
+
+bool findValue(int value, int* vector, int vectorSize){
+    bool find = false;
+    int i = 0;
+    while((i < vectorSize) && (find == false)){
+        if(vector[i] == value)
+            find = true;
+        i++;
+    }
+
+    return find;
+}
+
+int main() {
+    int* vector = generateRandomVector(1000000);
+
+   for (int i = 0; i < 1000000; i++) {
+        cout << vector[i] << ", ";
+    }
+    cout << endl;
+    
+
+    bool result = findValue(-1, vector, 1000000);
+
+    if (result)
+        cout << "Achou" << endl;
+    else
+        cout << "Nao achou" << endl;
+}
---
+++ implementacao-II-PAA/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ for (int i = 0; i < 1000000; i++) {
+        cout << vector[i] << ", ";
+    }
+    cout << endl;
