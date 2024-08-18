67c0b0c0d1b6f93e5bcf209c0d665b0b854346c5
NilsonDeon <nilsondeon01@gmail.com>
2024-08-18 19:15:11 -0300

    feat: Add functionality to modify vector elements and calculate operations

--- implementacao-II-PAA/main.cpp
+++ implementacao-II-PAA/main.cpp
@@ -10,16 +10,22 @@ int main() {
     int size = 100;
     int* vector = generateRandomVector(size);
 
-    for (int i = 0; i < size; i++) {
-        cout << vector[i] << ", ";
-    }
-    cout << endl;
-
-    int operations1 = findValueMethod1(1, vector, size);
-    int operations2 = findValueMethod2(1, vector);
-    int operations3 = findValueMethod3(1, vector, size);
-
+    vector[3] = 100;
+    int operations1 = findValueMethod1(100, vector, size);
+    int operations2 = findValueMethod2(100, vector);
+    int operations3 = findValueMethod3(100, vector, size);
     saveOperationsToCSV(operations1, operations2, operations3);
 
-    return 0;
+    vector[size / 2] = 101;
+    operations1 = findValueMethod1(101, vector, size);
+    operations2 = findValueMethod2(101, vector);
+    operations3 = findValueMethod3(101, vector, size);
+    saveOperationsToCSV(operations1, operations2, operations3);
+
+    vector[size - 3] = 102;
+    operations1 = findValueMethod1(102, vector, size);
+    operations2 = findValueMethod2(102, vector);
+    operations3 = findValueMethod3(102, vector, size);
+    saveOperationsToCSV(operations1, operations2, operations3);
+
 }
